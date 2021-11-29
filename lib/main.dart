29e71c29a718c795f8afa29722666bfcaa7e8b40
Ihar Paliashchuk <ihar.paliashchuk@itechart-group.com>
2021-11-29 17:12:17 +0800

    chore: update firestore and storage with app check for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:presentation/app.dart';
 
-void main() => runApp(const App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  //to provide app security when app check is enforced in firebase console
+  await FirebaseAppCheck.instance.activate();
+  runApp(const App());
+}
 
