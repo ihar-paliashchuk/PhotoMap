a6e987e1ef7009a25f63daa416fae32b1e0a7b6c
Ihar Paliashchuk <ihar.paliashchuk@itechart-group.com>
2021-12-02 13:36:23 +0800

    chore: create config

---
+++ core/lib/config/config.dart
@@ -0,0 +1,9 @@
+// Firebase Firestore
+
+const String rootCollection = 'photosData';
+const String userId = 'unique_user_id';
+const String userPhotos = 'user_photos';
+
+// Firebase Storage
+
+const String photoPholder = 'images/image-';
